ce3082f554b5ff4609d4dda31507d9c5143b7f67
Marina Soufi <xysamemir@gmail.com>
2022-11-18 15:06:13 +0100

    add react-dropdown & csvLink

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Dropdown from 'react-dropdown';
+import { CSVLink } from 'react-csv';
+
+
+const options = [
+  'one', 'two', 'three'
+];
+
+const salary = [
+  { firstname: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
+  { firstname: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
+  { firstname: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" }
+];
+
+const headers = [
+  { label: 'mounth', key: 'mounth' },
+  { label: 'salaryDate', key: 'salaryDate' },
+  { label: 'bonusDate', key: 'bonusDate' },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <Dropdown
+       className="w-80 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 absolute top-40 left-1/2 transform -translate-x-1/2 -translate-y-10"
+       menuClassName="py-1 text-base text-gray-700 dark:text-gray-200"
+       placeholder="Select an option"
+       options={options}/>
+
+       <CSVLink
+       className="py-4 px-4 bg-white text-blue-700 font-semibold rounded-sm shadow-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-100 focus:ring-opacity-75 inline-flex items-center"
+       data={salary}
+       headers={headers}
+       filename={'salary.csv'}
+       >
+         
+       </CSVLink>
+
+      
+      
+     
     </div>
   );
 }
